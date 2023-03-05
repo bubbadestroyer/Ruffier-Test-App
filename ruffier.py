c889467f102c68120942d0dff3a7d82449cec430
Egor <godenzo@vk.com>
2023-03-05 12:59:12 +0300

    Added class HeartCheck

--- ruffier.py
+++ ruffier.py
@@ -5,3 +5,25 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+
+
+
+
+
+
+class HeartCheck(App):
+    def build(self):
+        sm = ScreenManager()
+        # sm.add_widget(InstrScreen(name='instr'))
+        # sm.add_widget(PulseScr1(name='pulse1'))
+        # sm.add_widget(CheckSits(name='sits'))
+        # sm.add_widget(PulseScr2(name='pulse2'))
+        # sm.add_widget(Result(name='result'))
+
+        return sm
+
+
+
+
+app = HeartCheck()
+app.run()
